97fdd7408aa40a250b171a8b8ab2a859351875c1
Wisnu Kurniawan <kurniawan.wisnu23@gmail.com>
2020-10-12 10:26:40 +0700

    Init sqlite

---
+++ database.js
@@ -0,0 +1,60 @@
+import sqlite from 'sqlite3'
+
+//export.initDatabase
+export function initDatabase() {
+  return new sqlite.Database('data', (err) => {
+    if (err) {
+      throw err
+    }
+
+    console.log('Init DB Success')
+  })
+}
+
+/**
+ * init table
+ * @param {sqlite.Database} db 
+ */
+export function initTable(db) {
+  db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS product  (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      photo TEXT NOT NULL,
+      name VARCHAR(56) NOT NULL,
+      price INTEGER NOT NULL
+    );`)
+  })
+}
+
+/**
+ * 
+ * @param {sqlite.Database} db 
+ * @param {string} name 
+ * @param {number} price 
+ * @param {string} photo 
+ */
+export function insertProduct(db, name, price, photo) {
+  db.run('INSERT INTO product (photo,name,price) VALUES ($photo,$name,$price)', { $photo: photo, $name: name, $price: price }, (err) => {
+    if (err) {
+      throw err
+    }
+
+    console.log('product saved')
+  })
+}
+
+
+/**
+ * 
+ * @param {sqlite.Database} db 
+ */
+export function getProduct(db) {
+  db.all('SELECT * FROM product', (err, result) => {
+    if(err) {
+      console.log(err)
+      throw err
+    }
+    console.log(result)
+    return result
+  })
+}
