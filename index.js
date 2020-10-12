649b5fc6c1c36380603380a23086f600ca51accc
Wisnu Kurniawan <kurniawan.wisnu23@gmail.com>
2020-10-12 12:10:22 +0700

    Handle upload files & serve files

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ import hbs from 'hbs'
 import path from 'path'
 import morgan from 'morgan'
 import bodyParser from 'body-parser'
+import fileUpload from 'express-fileupload'
+import fs from 'fs'
 
 //const database = require('./database')
 import { initDatabase, initTable, insertProduct, getProduct } from './database.js'
-import { get } from 'http'
 
 const __dirname = path.resolve()
 
@@ -18,6 +19,9 @@ app.set('views', __dirname + '/layouts')
 app.set('view engine', 'html')
 app.engine('html', hbs.__express)
 
+// use file parser
+app.use(fileUpload())
+
 // log incoming reques
 app.use(morgan('combined'))
 
@@ -26,11 +30,11 @@ app.use(bodyParser.urlencoded())
 
 // serve static file
 app.use('/assets', express.static(__dirname + '/assets'))
+app.use('/files', express.static(__dirname + '/files'))
 
 app.get('/', (req, res, next) => {
   res.send({ success: true })
 })
-
 
 // get product list
 app.get('/product', async (req, res, next) => {
@@ -59,11 +63,23 @@ app.get('/add-product', (req, res, next) => {
 // handle form POST method
 app.post('/add-product', (req, res, next) => {
   console.log('Request', req.body)
-  // insert product
-  insertProduct(db, req.body.name, parseInt(req.body.price), '-')
-  
-  // redirect
-  res.redirect('/product')
+  console.log('File', req.files)
+  // get file name
+  const fileName = Date.now() + req.files.photo.name
+
+  // write file
+  fs.writeFile(path.join(__dirname, '/files/', fileName), req.files.photo.data, (err) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+
+    // insert product
+    insertProduct(db, req.body.name, parseInt(req.body.price), `/files/${fileName}`)
+    
+    // redirect
+    res.redirect('/product')
+  })
 })
 
 app.use((err, req, res, next) => {
