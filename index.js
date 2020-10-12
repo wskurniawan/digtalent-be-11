6c1c5b99013749094b5a0662084edd1fd98d8cfa
Wisnu Kurniawan <kurniawan.wisnu23@gmail.com>
2020-10-12 10:47:06 +0700

    init body parser & handle form data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import hbs from 'hbs'
 import path from 'path'
 import morgan from 'morgan'
+import bodyParser from 'body-parser'
 
 const __dirname = path.resolve()
 
@@ -14,6 +15,9 @@ app.engine('html', hbs.__express)
 // log incoming reques
 app.use(morgan('combined'))
 
+// parse request body
+app.use(bodyParser.urlencoded())
+
 // serve static file
 app.use('/assets', express.static(__dirname + '/assets'))
 
@@ -23,6 +27,17 @@ app.get('/', (req, res, next) => {
 
 app.get('/product', (req, res, next) => {
   res.render('product')
+})
+
+// handle form GET method
+app.get('/add-product', (req, res, next) => {
+  res.send(req.query)
+})
+
+// handle form POST method
+app.post('/add-product', (req, res, next) => {
+  console.log('Request', req.body)
+  res.send(req.body)
 })
 
 app.use((err, req, res, next) => {
