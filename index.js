b4eff187f28f6c51b92f69438220b31f21ac8a4d
Wisnu Kurniawan <kurniawan.wisnu23@gmail.com>
2020-10-12 09:55:13 +0700

    init layout & serve static files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import hbs from 'hbs'
 import path from 'path'
+import morgan from 'morgan'
 
 const __dirname = path.resolve()
 
@@ -10,8 +11,18 @@ app.set('views', __dirname + '/layouts')
 app.set('view engine', 'html')
 app.engine('html', hbs.__express)
 
+// log incoming reques
+app.use(morgan('combined'))
+
+// serve static file
+app.use('/assets', express.static(__dirname + '/assets'))
+
 app.get('/', (req, res, next) => {
   res.send({ success: true })
+})
+
+app.get('/product', (req, res, next) => {
+  res.render('product')
 })
 
 app.use((err, req, res, next) => {
